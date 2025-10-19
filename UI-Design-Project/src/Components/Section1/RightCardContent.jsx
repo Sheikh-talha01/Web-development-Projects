efe6d9dc35e8bcf6354149e63f849444c3400f74
Talha Sheikh <167472327+Sheikh-talha01@users.noreply.github.com>
2025-10-19 11:24:40 +0500

    adding new Project under a name "UI-Design-Project"

--- UI-Design-Project/src/Components/Section1/RightCardContent.jsx
+++ UI-Design-Project/src/Components/Section1/RightCardContent.jsx
@@ -4,7 +4,7 @@ const RightCardContent = ({ index, tag, intro, color }) => {
     const bg = color || '#2563eb'; // default to a blue if color is not provided
     return (
         <div className='absolute top-0 left-0 h-full w-full  p-8 flex flex-col justify-between'>
-            <h2 className='bg-white rounded-full h-12 w-12 flex justify-center items-center text-xl font-bold'>{index ?? ''}</h2>
+            <h2 className='bg-white text-black rounded-full h-12 w-12 flex justify-center items-center text-xl font-bold'>{index ?? ''}</h2>
             <div>
                 <p className='text-shadow-2xs text-lg leading-relaxed text-white mb-14'>{intro || 'Here is the detail of picture in which we have a lady professional sitting on a chair giving a bearutiful smile'}</p>
                 <div className='flex justify-between'>
